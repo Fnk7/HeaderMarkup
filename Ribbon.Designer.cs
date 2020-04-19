8ab7cff343e196b28d28a733d1df091b2130cccf
Fnk <34437956+Fnk7@users.noreply.github.com>
2020-04-19 22:01:38 +0800

    修改按钮名称

--- Ribbon.Designer.cs
+++ Ribbon.Designer.cs
@@ -41,9 +41,11 @@
             this.grpAnnotate = this.Factory.CreateRibbonGroup();
             this.btMarkTable = this.Factory.CreateRibbonButton();
             this.btMarkHeader = this.Factory.CreateRibbonButton();
+            this.separator1 = this.Factory.CreateRibbonSeparator();
             this.btMarkData = this.Factory.CreateRibbonButton();
             this.btMarkTitle = this.Factory.CreateRibbonButton();
             this.labelNot = this.Factory.CreateRibbonLabel();
+            this.separator3 = this.Factory.CreateRibbonSeparator();
             this.btMarkDataHeader = this.Factory.CreateRibbonButton();
             this.btMarkTitleHeader = this.Factory.CreateRibbonButton();
             this.labelSeem = this.Factory.CreateRibbonLabel();
@@ -52,9 +54,9 @@
             this.btDeleteMark = this.Factory.CreateRibbonButton();
             this.btDeleteTable = this.Factory.CreateRibbonButton();
             this.group1 = this.Factory.CreateRibbonGroup();
-            this.btTODO1 = this.Factory.CreateRibbonButton();
-            this.btTODO2 = this.Factory.CreateRibbonButton();
-            this.btTODO3 = this.Factory.CreateRibbonButton();
+            this.btRF = this.Factory.CreateRibbonButton();
+            this.btSVM = this.Factory.CreateRibbonButton();
+            this.btNN = this.Factory.CreateRibbonButton();
             this.groupSettings = this.Factory.CreateRibbonGroup();
             this.btSettings = this.Factory.CreateRibbonButton();
             this.tabHMarkup.SuspendLayout();
@@ -103,9 +105,11 @@
             // 
             this.grpAnnotate.Items.Add(this.btMarkTable);
             this.grpAnnotate.Items.Add(this.btMarkHeader);
+            this.grpAnnotate.Items.Add(this.separator1);
             this.grpAnnotate.Items.Add(this.btMarkData);
             this.grpAnnotate.Items.Add(this.btMarkTitle);
             this.grpAnnotate.Items.Add(this.labelNot);
+            this.grpAnnotate.Items.Add(this.separator3);
             this.grpAnnotate.Items.Add(this.btMarkDataHeader);
             this.grpAnnotate.Items.Add(this.btMarkTitleHeader);
             this.grpAnnotate.Items.Add(this.labelSeem);
@@ -134,6 +138,10 @@
             this.btMarkHeader.ShowImage = true;
             this.btMarkHeader.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btMark_Click);
             // 
+            // separator1
+            // 
+            this.separator1.Name = "separator1";
+            // 
             // btMarkData
             // 
             this.btMarkData.Image = global::HeaderMarkup.Properties.Resources.data;
@@ -154,6 +162,10 @@
             // 
             this.labelNot.Label = "Not Header";
             this.labelNot.Name = "labelNot";
+            // 
+            // separator3
+            // 
+            this.separator3.Name = "separator3";
             // 
             // btMarkDataHeader
             // 
@@ -206,35 +218,35 @@
             // 
             // group1
             // 
-            this.group1.Items.Add(this.btTODO1);
-            this.group1.Items.Add(this.btTODO2);
-            this.group1.Items.Add(this.btTODO3);
+            this.group1.Items.Add(this.btRF);
+            this.group1.Items.Add(this.btSVM);
+            this.group1.Items.Add(this.btNN);
             this.group1.Label = "Predict";
             this.group1.Name = "group1";
             // 
-            // btTODO1
+            // btRF
             // 
-            this.btTODO1.Image = global::HeaderMarkup.Properties.Resources.run;
-            this.btTODO1.Label = "Generate CSV";
-            this.btTODO1.Name = "btTODO1";
-            this.btTODO1.ShowImage = true;
-            this.btTODO1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonGenerateCSV_Click);
+            this.btRF.Image = global::HeaderMarkup.Properties.Resources.run;
+            this.btRF.Label = "Random Forest";
+            this.btRF.Name = "btRF";
+            this.btRF.ShowImage = true;
+            this.btRF.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonGenerateCSV_Click);
             // 
-            // btTODO2
+            // btSVM
             // 
-            this.btTODO2.Image = global::HeaderMarkup.Properties.Resources.run;
-            this.btTODO2.Label = "Train Model";
-            this.btTODO2.Name = "btTODO2";
-            this.btTODO2.ShowImage = true;
-            this.btTODO2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonTrain_Click);
+            this.btSVM.Image = global::HeaderMarkup.Properties.Resources.run;
+            this.btSVM.Label = "Naive Bayes";
+            this.btSVM.Name = "btSVM";
+            this.btSVM.ShowImage = true;
+            this.btSVM.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.buttonTrain_Click);
             // 
-            // btTODO3
+            // btNN
             // 
-            this.btTODO3.Image = global::HeaderMarkup.Properties.Resources.run;
-            this.btTODO3.Label = "Predict Header";
-            this.btTODO3.Name = "btTODO3";
-            this.btTODO3.ShowImage = true;
-            this.btTODO3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btPredict_Click);
+            this.btNN.Image = global::HeaderMarkup.Properties.Resources.run;
+            this.btNN.Label = "Neural Net";
+            this.btNN.Name = "btNN";
+            this.btNN.ShowImage = true;
+            this.btNN.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btPredict_Click);
             // 
             // groupSettings
             // 
@@ -290,11 +302,13 @@
         internal Microsoft.Office.Tools.Ribbon.RibbonButton btSettings;
         internal Microsoft.Office.Tools.Ribbon.RibbonButton btDeleteWorkbook;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupSettings;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton btTODO2;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton btTODO3;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton btTODO1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btSVM;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btNN;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btRF;
         internal Microsoft.Office.Tools.Ribbon.RibbonLabel labelSeem;
         internal Microsoft.Office.Tools.Ribbon.RibbonLabel labelNot;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonSeparator separator3;
     }
 
     partial class ThisRibbonCollection
